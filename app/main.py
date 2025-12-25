05640fbf1d57161cf185be7b7f2580a6dd7c9711
StarCode Kh <starcodekh@email.com>
2025-12-25 09:37:05 +0700

    user crud

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.auth import register, login, profile, logout
+from app.api.users import user_crud
 
 app = FastAPI()
 
@@ -7,3 +8,5 @@ app.include_router(register.router, prefix="/api/auth")
 app.include_router(login.router, prefix="/api/auth")
 app.include_router(profile.router, prefix="/api/auth")
 app.include_router(logout.router, prefix="/api/auth")
+
+app.include_router(user_crud.router, prefix="/api")
